fb62a25ba1de7cb9ce82305a65eb33c9c3c7c7e2
nimmyroz06 <nimmyfisat2023@gmail.com>
2024-09-29 20:02:07 +0530

    signup api completed

---
+++ models/users.js
@@ -0,0 +1,32 @@
+const { default: mongoose } = require("mongoose")
+const Mongoose=require("mongoose")
+
+const userSchema=Mongoose.Schema(
+    {
+        name:{
+            type:String,
+            required:true
+        },
+        phone:{
+            type:String,
+            required:true
+        },
+        email:{
+            type:String,
+            required:true
+        },
+        district:{
+            type:String,
+            required:true
+        },
+        password:{
+            type:String,
+            required:true
+        },
+
+
+    }
+)
+
+var userModel= Mongoose.model("users",userSchema)
+module.exports=userModel
